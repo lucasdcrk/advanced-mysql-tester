f57e236745aaa3cc9bf2cae66079dd39ad8cecd9
Lucas Decrock <lucas@decrock.me>
2023-12-16 00:17:34 +0100

    Improve async

--- src/index.js
+++ src/index.js
@@ -26,13 +26,18 @@ if (compare) {
 }
 
 const queries = {};
+
 fs.readdirSync('./src/queries').forEach((file) => {
   const queryName = file.split('.')[0];
   queries[queryName] = fs.readFileSync(`./src/queries/${file}`, 'utf8');
 });
 
-async function runQueries(db) {
-  const queryPromises = Object.keys(queries).map((queryName) => {
+function getPromises(db, name) {
+  if (!db) {
+    return [];
+  }
+
+  return Object.keys(queries).map((queryName) => {
     return new Promise((resolve, reject) => {
       const query = queries[queryName];
       const start = new Date().getTime();
@@ -42,31 +47,45 @@ async function runQueries(db) {
           reject(err);
         } else {
           const end = new Date().getTime();
-          console.log(`${queryName}: ${end - start}ms`);
-          resolve();
+
+          console.log(`[${name}] Completed ${queryName} in ${end - start}ms`);
+
+          resolve({
+            query: queryName,
+            time: end - start,
+          });
         }
       });
     });
   });
+}
+
+async function runQueries() {
+  const db1Promises = getPromises(db1, 'DB1');
+  const db2Promises = getPromises(db2, 'DB2');
 
   try {
-    await Promise.all(queryPromises);
-    console.log('All queries completed.');
+    const db1Results = await Promise.all(db1Promises);
+    const db2Results = await Promise.all(db2Promises);
+
+    console.log('All queries executed.');
+    console.log('-----------------');
+    console.log('DB1 Results:');
+    db1Results?.map((result) => {
+      console.log(`${result.query}: ${result.time}ms`);
+    });
+    if (compare) {
+      console.log('-----------------');
+      console.log('DB2 Results:');
+      db2Results?.map((result) => {
+        console.log(`${result.query}: ${result.time}ms`);
+      });
+    }
   } catch (error) {
     console.error('Error executing queries:', error);
   }
 }
 
-if (compare) {
-  console.log('Running queries on multiple databases...');
-}
-
-runQueries(db1).then(() => {
-  if (compare) {
-    runQueries(db2).then(() => {
-      process.exit();
-    });
-  } else {
-    process.exit();
-  }
+runQueries().then(results => {
+  process.exit();
 });
